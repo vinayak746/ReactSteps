96cc3e6d038a30e97ccd70a17141de7d37fb2cbb
Vinayak Arora <vinayakarora7461@gmail.com>
2025-05-14 18:44:53 +0530

    fix: update import statements to include file extensions for Home and About components

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
 
 import "./App.css";
 
