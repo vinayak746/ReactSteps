22d64f1a18a22f0a80b9275e9057a215bef3fa3e
Vinayak Arora <vinayakarora7461@gmail.com>
2025-06-14 12:42:53 +0530

    feat: add Appwrite integration for updating search count and implement search count logic in App component

--- Buffering/appwrite.js
+++ Buffering/appwrite.js
@@ -1,5 +1,37 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
-export const updateSearchCount = async (searchTerm, movie) => {};
+const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  console.log("Function triggered with:", searchTerm, movie);
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
+
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.error("Error updating search count:", error);
+  }
+};
--- Buffering/src/App.jsx
+++ Buffering/src/App.jsx
@@ -53,7 +53,9 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
-      updateSearchCount();
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error("Error fetching movies:", error);
       setErrorMessage("Failed to fetch movies. Please try again later.");
